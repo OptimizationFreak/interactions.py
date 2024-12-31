e02e5c825bbbd4d407aa8757e24ebf58e16b05be
Arlo <arlobusinessmain@gmail.com>
2024-12-30 23:39:19 -0500

    fix 2

--- interactions/models/discord/application.py
+++ interactions/models/discord/application.py
@@ -100,7 +100,7 @@ class Application(DiscordObject):
     async def fetch_emoji(self, emoji_id: Snowflake_Type) -> PartialEmoji:
         """Fetch an emoji for this application"""
         response = await self._client.http.get_application_emoji(self.id, emoji_id)
-        return await self._client.cache.place_emoji_data(None, response)
+        return self._client.cache.place_emoji_data(None, response)
 
     async def create_emoji(self, name: str, imagefile: UPLOADABLE_TYPE) -> PartialEmoji:
         """Create an emoji for this application"""
